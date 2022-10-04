593dc4a644c8a8e616304a48759a2da561b7b56d
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-10-04 16:35:57 +0500

    ♻️ Update : counter Increment counter button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = { 
-        count: 1,
+        count: 0,
+        tags: ['tags']
      } 
+     renderTags(){
+        if(this.state.tags.length === 0) return <p>There is no tag.!</p>
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+        
+     }
+     handleIncrement = product => {
+        console.log(product);
+        this.setState({count: this.state.count + 1});
+     }
     render() { 
         return (
         <div className="container">
+            {this.state.tags.length === 0 && 'Please Create New Tags!'}
             <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
-            <button className='btn btn-outline-primary btn-sm'>Increment</button>
+            <button onClick={() => this.handleIncrement({id:1})} className='btn btn-outline-primary btn-sm'>Increment</button>
+           {/* {this.renderTags()} */}
         </div>
         );
     }
